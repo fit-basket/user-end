0f4ed8ee834ab6b2f80b73caa7d1045e81ba87a1
souvikdeb2612 <souvikdeb26@gmail.com>
2023-09-29 10:58:27 +0530

    zoom backend testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,12 +23,24 @@
 
 // App.js
 
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OAuth from "./OAuth"; // The OAuth component from Step 2
 import OAuthInitiation from "./OAuthInitiation"; // The OAuthInitiation component from Step 3
 
 function App() {
+  // Inside any React component, perhaps inside a useEffect hook
+
+  // const fetchData = async () => {
+  //   const response = await fetch("http://localhost:3001/api/hello");
+  //   const data = await response.json();
+  //   console.log(data);
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
   return (
     <div className="App">
       <Router>
