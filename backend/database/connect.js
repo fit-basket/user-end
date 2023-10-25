1dca27edb16ce31c00e067f17cfb18be92cf424f
souvikdeb2612 <souvikdeb26@gmail.com>
2023-10-25 14:14:44 +0530

    add mongoose

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 
 const uri =
   "mongodb+srv://souvikdeb:vlls7mKtjw8Fs4Be@cluster0.ylcnuqm.mongodb.net/thePurpleMango?retryWrites=true&w=majority";
@@ -6,15 +7,24 @@ const uri =
 let dbConnection;
 
 const connectToDb = (cb) => {
-  MongoClient.connect(uri)
-    .then((client) => {
-      dbConnection = client.db();
-      return cb();
-    })
-    .catch((err) => {
-      console.log(err);
-      return cb(err);
-    });
+  mongoose.connect(uri);
+  // .then((client) => {
+  //   dbConnection = client.db();
+  //   return cb();
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  //   return cb(err);
+  // });
+  // MongoClient.connect(uri)
+  //   .then((client) => {
+  //     dbConnection = client.db();
+  //     return cb();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     return cb(err);
+  //   });
 };
 
 const getDatabase = () => dbConnection;
