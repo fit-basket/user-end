099227a5eb3909ddf0812aebdcfa145e5e6f6998
souvikdeb2612 <souvikdeb26@gmail.com>
2023-11-11 20:42:43 +0530

    cookie fixes

--- frontend/src/utils/base.js
+++ frontend/src/utils/base.js
@@ -2,7 +2,7 @@ let baseUrl;
 
 if (process.env.REACT_APP_ENVIRONMENT === "dev") {
   baseUrl = "http://localhost:3001/api";
-} else {
+} else if (process.env.REACT_APP_ENVIRONMENT === "prod") {
   baseUrl = "https://the-purple-mango.onrender.com/api";
 }
 
